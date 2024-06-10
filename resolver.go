7b94eb356d1f19b94329d2a2dd052c7fcbd7c1d1
Will McCutchen <will@mccutch.org>
2024-06-10 16:45:27 -0400

    Refactor and improve name server resolution
    We now capture more structured info about each name server and properly
    parse "glue" NS records.
    
    Fixes #1.

--- resolver.go
+++ resolver.go
@@ -15,20 +15,20 @@ import (
 // https://www.iana.org/domains/root/servers
 //
 // may be overridden in tests
-var defaultRootNameServers = []string{
-	"198.41.0.4",     // a.root-servers.net Verisign, Inc.
-	"199.9.14.201",   // b.root-servers.net University of Southern California, Information Sciences Institute
-	"192.33.4.12",    // c.root-servers.net Cogent Communications
-	"199.7.91.13",    // d.root-servers.net University of Maryland
-	"192.203.230.10", // e.root-servers.net NASA (Ames Research Center)
-	"192.5.5.241",    // f.root-servers.net Internet Systems Consortium, Inc.
-	"192.112.36.4",   // g.root-servers.net US Department of Defense (NIC)
-	"198.97.190.53",  // h.root-servers.net US Army (Research Lab)
-	"192.36.148.17",  // i.root-servers.net Netnod
-	"192.58.128.30",  // j.root-servers.net Verisign, Inc.
-	"193.0.14.129",   // k.root-servers.net RIPE NCC
-	"199.7.83.42",    // l.root-servers.net ICANN
-	"202.12.27.33",   // m.root-servers.net WIDE Project
+var defaultRootNameServers = []nameServerDef{
+	newNameServerDef("a.root-servers.net", ".", net.ParseIP("198.41.0.4")),     // Verisign, Inc.
+	newNameServerDef("b.root-servers.net", ".", net.ParseIP("199.9.14.201")),   // University of Southern California, Information Sciences Institute
+	newNameServerDef("c.root-servers.net", ".", net.ParseIP("192.33.4.12")),    // Cogent Communications
+	newNameServerDef("d.root-servers.net", ".", net.ParseIP("199.7.91.13")),    // University of Maryland
+	newNameServerDef("e.root-servers.net", ".", net.ParseIP("192.203.230.10")), // NASA (Ames Research Center)
+	newNameServerDef("f.root-servers.net", ".", net.ParseIP("192.5.5.241")),    // Internet Systems Consortium, Inc.
+	newNameServerDef("g.root-servers.net", ".", net.ParseIP("192.112.36.4")),   // US Department of Defense (NIC)
+	newNameServerDef("h.root-servers.net", ".", net.ParseIP("198.97.190.53")),  // US Army (Research Lab)
+	newNameServerDef("i.root-servers.net", ".", net.ParseIP("192.36.148.17")),  // Netnod
+	newNameServerDef("j.root-servers.net", ".", net.ParseIP("192.58.128.30")),  // Verisign, Inc.
+	newNameServerDef("k.root-servers.net", ".", net.ParseIP("193.0.14.129")),   // RIPE NCC
+	newNameServerDef("l.root-servers.net", ".", net.ParseIP("199.7.83.42")),    // ICANN
+	newNameServerDef("m.root-servers.net", ".", net.ParseIP("202.12.27.33")),   // WIDE Project
 }
 
 // New returns a new Resolver.
@@ -55,14 +55,14 @@ func New(opts *Opts) *Resolver {
 
 // Opts defines the options used to configure a Resolver.
 type Opts struct {
-	RootNameServers []string
+	RootNameServers []nameServerDef
 	Dialer          *net.Dialer
 	Logger          *slog.Logger
 }
 
 // Resolver makes DNS queries.
 type Resolver struct {
-	rootNameServers []string
+	rootNameServers []nameServerDef
 	dialer          *net.Dialer
 	logger          *slog.Logger
 
@@ -77,17 +77,17 @@ func (r *Resolver) LookupIP(ctx context.Context, domainName string) ([]net.IP, e
 	return result, err
 }
 
-func (r *Resolver) doLookupIP(ctx context.Context, nameServer string, domainName string, depth int) ([]net.IP, int, error) {
+func (r *Resolver) doLookupIP(ctx context.Context, nameServer nameServerDef, domainName string, depth int) ([]net.IP, int, error) {
 	msg, err := r.sendQuery(ctx, nameServer, domainName, RecordTypeA, depth)
 	if err != nil {
 		return nil, depth, err
 	}
 
 	r.logRecords("answer", msg.Answers, depth)
-	r.logRecords("additional", msg.Additionals, depth)
 	r.logRecords("authority", msg.Authorities, depth)
+	r.logRecords("additional", msg.Additionals, depth)
 
-	// successfully resolved IP address, we're done
+	// if we successfully resolved IP address(es), we're done
 	ips, err := ipAddrsFromRecords(msg.Answers)
 	if err != nil {
 		return nil, depth, err
@@ -96,80 +96,80 @@ func (r *Resolver) doLookupIP(ctx context.Context, nameServer string, domainName
 		return ips, depth, nil
 	}
 
-	// resolve again with a new name server from the response
-	nsIPs, err := ipAddrsFromRecords(msg.Additionals)
-	if err != nil {
-		return nil, depth, err
-	}
-	if len(nsIPs) > 0 {
-		nameServer = randomChoice(nsIPs).String()
+	// if we find glue NS records, re-resolve again with a new name server
+	if glue, err := getGlueNameServers(msg); err != nil {
+		return nil, depth, fmt.Errorf("failed to get glue nameservers: %w", err)
+	} else if len(glue) > 0 {
+		nameServer = randomChoice(glue)
 		r.logger.Debug(
-			"recursively resolving with new name server",
+			"recursively resolving with new name server from glue records",
 			slog.Int("depth", depth),
-			slog.String("ns_addr", nameServer),
-			slog.Int("ns_addr_count", len(nsIPs)),
-			slog.String("target_domain", domainName),
+			slog.String("query_name", domainName),
+			slog.String("ns_name", nameServer.name),
+			slog.String("ns_addr", nameServer.addr.String()),
+			slog.String("ns_authority", nameServer.authority),
 		)
 		return r.doLookupIP(ctx, nameServer, domainName, depth+1)
 	}
 
-	// first resolve nameserver domain to nameserver IP, then recurse with
-	// new nameserver IP
-	if nsDomains := domainsFromRecords(msg.Authorities, RecordTypeNS); len(nsDomains) > 0 {
-		nsDomain := randomChoice(nsDomains)
+	// if we find NS records but no glue records, we must first resolve
+	// nameserver domain to nameserver IP, then recurse with new nameserver IP
+	if ns, found := matchRecord(msg.Authorities, RecordTypeNS); found {
+		nsDomain := string(ns.Data)
 		r.logger.Debug(
 			"resolving NS domain",
 			slog.Int("depth", depth),
 			slog.String("ns_domain", nsDomain),
-			slog.Int("ns_domain_count", len(nsDomains)),
-			slog.String("target_domain", domainName),
 		)
-		nextNSAddrs, newDepth, err := r.doLookupIP(ctx, nameServer, nsDomain, depth+1)
+		nextNSAddrs, newDepth, err := r.doLookupIP(ctx, r.chooseRootNameServer(), nsDomain, depth+1)
 		if err != nil {
 			return nil, newDepth, fmt.Errorf("error resolving nameserver: %w", err)
 		}
-		if len(nextNSAddrs) > 0 {
-			for _, nsAddr := range nextNSAddrs {
-				if nsAddr.IsPrivate() {
-					r.logger.Debug("skipping private name server", slog.String("ns_addr", nsAddr.String()))
-					continue
-				}
-				nameServer = nsAddr.String()
-				r.logger.Debug(
-					"recursively resolving with new name server",
-					slog.Int("depth", depth),
-					slog.String("target_domain", domainName),
-					slog.String("ns_addr", nameServer),
-					slog.Int("ns_addr_count", len(nextNSAddrs)),
-				)
-				return r.doLookupIP(ctx, nameServer, domainName, newDepth+1)
+		if len(nextNSAddrs) == 0 {
+			return nil, newDepth, fmt.Errorf("no IP addresses found for nameserver %q", nsDomain)
+		}
+		for _, nsAddr := range nextNSAddrs {
+			if nsAddr.IsPrivate() {
+				r.logger.Debug("skipping private name server", slog.String("ns_addr", nsAddr.String()))
+				continue
 			}
+			nameServer = newNameServerDef(nsDomain, string(ns.Name), nsAddr)
+			r.logger.Debug(
+				"recursively resolving with new name server",
+				slog.Int("depth", depth),
+				slog.String("query_domain", domainName),
+				slog.String("ns_name", nameServer.name),
+				slog.String("ns_addr", nameServer.addr.String()),
+				slog.String("ns_authority", nameServer.authority),
+			)
+			return r.doLookupIP(ctx, nameServer, domainName, newDepth+1)
 		}
 	}
 
-	if cnameDomains := domainsFromRecords(msg.Answers, RecordTypeCNAME); len(cnameDomains) > 0 {
-		cnameDomain := randomChoice(cnameDomains)
+	// finally, if we find a CNAME, recursively resolve it instead of our
+	// current query
+	if cname, found := matchRecord(msg.Answers, RecordTypeCNAME); found {
+		cnameDomain := string(cname.Data)
 		r.logger.Debug(
 			"recursively resolving CNAME",
 			slog.Int("depth", depth),
 			slog.String("cname", cnameDomain),
-			slog.Int("cname_count", len(cnameDomains)),
-			slog.String("target_domain", domainName),
+			slog.String("query_name", domainName),
 		)
 		return r.doLookupIP(ctx, nameServer, cnameDomain, depth+1)
 	}
 
 	r.logger.Debug(
 		"no IP addresses found",
-		slog.String("target_domain", domainName),
+		slog.String("query_name", domainName),
 		slog.String("msg", fmt.Sprintf("%#v", msg)),
 	)
 	return nil, depth, fmt.Errorf("failed to resolve %s to an IP", domainName)
 }
 
 // sendQuery queries a DNS server for a domain and parses the response.
-func (r *Resolver) sendQuery(ctx context.Context, nameserver string, targetDomain string, recordType RecordType, depth int) (Message, error) {
-	nsAddr := net.JoinHostPort(nameserver, "53")
+func (r *Resolver) sendQuery(ctx context.Context, nameServer nameServerDef, targetDomain string, recordType RecordType, depth int) (Message, error) {
+	nsAddr := net.JoinHostPort(nameServer.addr.String(), "53")
 	conn, err := r.dialer.DialContext(ctx, "udp", nsAddr)
 	if err != nil {
 		return Message{}, fmt.Errorf("failed to dial nameserver %q: %w", nsAddr, err)
@@ -178,8 +178,10 @@ func (r *Resolver) sendQuery(ctx context.Context, nameserver string, targetDomai
 	r.logger.Debug(
 		"sending DNS query to nameserver",
 		slog.Int("depth", depth),
-		slog.String("nameserver", nameserver),
-		slog.String("target_domain", targetDomain),
+		slog.String("query_name", targetDomain),
+		slog.String("ns_name", nameServer.name),
+		slog.String("ns_addr", nameServer.addr.String()),
+		slog.String("ns_authority", nameServer.authority),
 		slog.String("resource_type", recordType.String()),
 	)
 
@@ -195,7 +197,7 @@ func (r *Resolver) sendQuery(ctx context.Context, nameserver string, targetDomai
 	}
 
 	resp := buf[:n]
-	r.logger.Debug("raw DNS response bytes", slog.String("resp_bytes", string(resp)))
+	// r.logger.Debug("raw DNS response bytes", slog.String("resp_bytes", string(resp)))
 
 	msg, err := parseMessage(byteview.New(resp))
 	if err != nil {
@@ -203,8 +205,10 @@ func (r *Resolver) sendQuery(ctx context.Context, nameserver string, targetDomai
 			"failed to parse DNS response",
 			slog.Int("depth", depth),
 			slog.String("err", err.Error()),
-			slog.String("target_domain", targetDomain),
-			slog.String("server", nameserver),
+			slog.String("query_name", targetDomain),
+			slog.String("ns_name", nameServer.name),
+			slog.String("ns_addr", nameServer.addr.String()),
+			slog.String("ns_authority", nameServer.authority),
 			slog.String("resource_type", recordType.String()),
 		)
 		return Message{}, err
@@ -215,7 +219,7 @@ func (r *Resolver) sendQuery(ctx context.Context, nameserver string, targetDomai
 
 // chooseRootNameServer chooses an authoritative root name server in round-robin
 // fashion.
-func (r *Resolver) chooseRootNameServer() string {
+func (r *Resolver) chooseRootNameServer() nameServerDef {
 	idx := r.rootNameServerIdx.Add(1)
 	return r.rootNameServers[int(idx)%len(r.rootNameServers)]
 }
@@ -243,6 +247,51 @@ func (r *Resolver) logRecords(section string, records []Record, depth int) {
 	}
 }
 
+// getGlueNameServers joins the glue records in the additional section with the
+// name servers in the authority section.
+func getGlueNameServers(msg Message) ([]nameServerDef, error) {
+	if len(msg.Additionals) == 0 {
+		return nil, nil
+	}
+
+	authorityIdx := make(map[string]int)
+	for i, a := range msg.Authorities {
+		authorityIdx[string(a.Data)] = i
+	}
+
+	results := make([]nameServerDef, 0, len(msg.Additionals))
+	for _, a := range msg.Additionals {
+		if a.Type != RecordTypeA && a.Type != RecordTypeAAAA {
+			return nil, fmt.Errorf("unexpected record type %s (%v) in additional section", a.Type, a.Type)
+		}
+
+		idx, found := authorityIdx[string(a.Name)]
+		if !found {
+			return nil, fmt.Errorf("no authority found for %q", a.Name)
+		}
+
+		addrs, err := parseIPAddrs(a.Type, a.Data)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse glue IP address: %w", err)
+		}
+
+		authority := msg.Authorities[idx]
+		ns := newNameServerDef(string(authority.Data), string(authority.Name), addrs[0])
+		results = append(results, ns)
+	}
+	return results, nil
+}
+
+// matchRecord returns the first record of the given type in the given slice.
+func matchRecord(records []Record, recordType RecordType) (Record, bool) {
+	for _, r := range records {
+		if r.Type == recordType {
+			return r, true
+		}
+	}
+	return Record{}, false
+}
+
 func ipAddrsFromRecords(records []Record) ([]net.IP, error) {
 	results := make([]net.IP, 0, len(records))
 	for _, r := range records {
@@ -257,17 +306,17 @@ func ipAddrsFromRecords(records []Record) ([]net.IP, error) {
 	return results, nil
 }
 
-func domainsFromRecords(records []Record, targetRecordType RecordType) []string {
-	results := make([]string, 0, len(records))
-	for _, a := range records {
-		if a.Type == targetRecordType {
-			results = append(results, string(a.Data))
-		}
-	}
-	return results
-}
-
 // randomChoice returns a random element from the given slice.
 func randomChoice[T any](choices []T) T {
 	return choices[rand.Intn(len(choices))]
+}
+
+type nameServerDef struct {
+	name      string
+	addr      net.IP
+	authority string
+}
+
+func newNameServerDef(name string, authority string, addr net.IP) nameServerDef {
+	return nameServerDef{addr: addr, name: name, authority: authority}
 }
